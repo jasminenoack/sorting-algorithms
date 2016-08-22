3ea6861641ddd2063f8ba35d63a18cab0553f99a
Jasmine Noack <jasminenoack@gmail.com>
2016-08-22 19:53:17 -0400

    add templating

--- js/bogo.js
+++ js/bogo.js
@@ -1,46 +1,50 @@
-function setup () {
-    setupArray()
-    superSetup()
-}
+window.bogo =  {
+    title: "Bogo Sort",
 
-function checkSort() {
-    for (var i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false
+    setup: function () {
+        setupArray()
+        superSetup()
+    },
+
+    checkSort: function () {
+        for (var i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false
+            }
         }
-    }
-    return true
-}
+        return true
+    },
 
-function next() {
-    if (stop) {
-        stop = false;
-        running = false;
-        return
-    }
-    var sorted = checkSort()
-    if (sorted) {
-        running = false;
-        return
-    }
-    if (index === 0) {
-        index = arr.length - 1
-        return next(arr.length - 1)
-    }
-    $lis = $("li")
-    var randNum = Math.floor(Math.random() * arr.length);
-    var cellValue = arr[index];
-    arr[index] = arr[randNum];
-    arr[randNum] = cellValue;
+    next: function () {
+        if (stop) {
+            stop = false;
+            running = false;
+            return
+        }
+        var sorted = bogo.checkSort()
+        if (sorted) {
+            running = false;
+            return
+        }
+        if (index === 0) {
+            index = arr.length - 1
+            return bogo.next(arr.length - 1)
+        }
+        $lis = $("li")
+        var randNum = Math.floor(Math.random() * arr.length);
+        var cellValue = arr[index];
+        arr[index] = arr[randNum];
+        arr[randNum] = cellValue;
 
-    setupLis(index, randNum);
-    $lis.removeClass("current");
-    $($lis[index]).addClass("current")
-    index--
-    setTimeout(next, delay)
-}
+        bogo.setupLis(index, randNum);
+        $lis.removeClass("current");
+        $($lis[index]).addClass("current")
+        index--
+        setTimeout(bogo.next, delay)
+    },
 
-function setupLis(a, b) {
-    $($lis[a]).css("bottom", arr[a] * multiplier + "px")
-    $($lis[b]).css("bottom", arr[b] * multiplier + "px")
+    setupLis: function (a, b) {
+        $($lis[a]).css("bottom", arr[a] * multiplier + "px")
+        $($lis[b]).css("bottom", arr[b] * multiplier + "px")
+    }
 }
---
+++ js/sort.js
@@ -0,0 +1,129 @@
+"use strict"
+var width = 40;
+var multiplier = 10;
+var running = false;
+var arr = [];
+var delay = 20;
+var stop = false;
+var program = {};
+
+$(document).ready(function () {
+    var $template = $("#sort");
+    var compiled = _.template($template.html());
+    program = window[window.location.search.replace("?", "")]
+    $("body").html(compiled({
+        name: program.title,
+        delay: delay
+    }));
+    $(".reset").click(stopAndReset);
+    $(".card header").click(
+        function (event) {
+            event.stopPropagation();
+            $(event.currentTarget).parent().toggleClass("footer-hidden")
+        }
+    );
+    $(".x-x-sm").click(function () {
+        $(".content").removeClass().addClass("content item-x-x-sm")
+        width = 400;
+        multiplier = 1;
+        stopAndReset();
+    });
+    $(".x-sm").click(function () {
+        $(".content").removeClass().addClass("content item-x-sm")
+        width = 200;
+        multiplier = 2;
+        stopAndReset();
+    });
+    $(".sm").click(function () {
+        $(".content").removeClass().addClass("content item-sm")
+        width = 80;
+        multiplier = 5;
+        stopAndReset();
+    });
+    $(".md").click(function () {
+        $(".content").removeClass().addClass("content item-md")
+        width = 40;
+        multiplier = 10;
+        stopAndReset();
+    });
+    $(".lg").click(function () {
+        $(".content").removeClass().addClass("content item-lg")
+        width = 20;
+        multiplier = 20;
+        stopAndReset();
+    });
+    $(".x-lg").click(function () {
+        $(".content").removeClass().addClass("content item-x-lg")
+        width = 10;
+        multiplier = 40;
+        stopAndReset();
+    });
+    $(".x-x-lg").click(function () {
+        $(".content").removeClass().addClass("content item-x-x-lg")
+        width = 5;
+        multiplier = 80;
+        stopAndReset();
+    });
+    $(".faster").click(function () {
+        delay = Math.min(delay, delay - 10);
+        $(".delay").text(delay + " milliseconds")
+    });
+    $(".slower").click(function () {
+        delay += 10;
+        $(".delay").text(delay + " milliseconds")
+    });
+
+    program.setup();
+});
+
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+function setupArray () {
+    arr = Array.apply(null, {length: width}).map(Number.call, Number)
+}
+
+function superSetup(firstIndex) {
+    shuffle(arr)
+    var $content = $(".content");
+    $content.html("")
+    for(var i = 0; i < arr.length; i++) {
+        var cellValue = arr[i];
+        $content.append(
+            "<li style=left:"+i*multiplier+"px;bottom:"+cellValue*multiplier+"px></li>"
+        )
+    }
+    var $lis = $("li")
+    $lis.removeClass("current")
+    $($lis[0]).addClass("current")
+    window.sorted = true;
+    window.index = firstIndex || 0;
+    if (!running) {
+        running = true;
+        setTimeout(program.next, delay)
+    }
+}
+
+function resetIfStopped () {
+    if (!stop && !running) {
+        program.setup()
+    } else {
+        setTimeout(resetIfStopped, 5);
+    }
+}
+
+function stopAndReset() {
+    if (!running) {
+        program.setup();
+    } else {
+        stop = true;
+        setTimeout(resetIfStopped, 5);
+    }
+}

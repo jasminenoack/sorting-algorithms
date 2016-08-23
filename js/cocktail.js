e9993e15f9ee63cfefbb4a7545c372ee13c827c7
Jasmine Noack <jasminenoack@gmail.com>
2016-08-22 20:21:43 -0400

    cocktail

--- js/cocktail.js
+++ js/cocktail.js
@@ -1,50 +1,54 @@
-function setup () {
-    setupArray()
-    window.maxIndex = arr.length;
-    window.minIndex = 0;
-    window.direction = 1;
-    superSetup()
-}
+window.cocktail = {
+    title: "Cocktail Sort",
 
-function next () {
-    if (stop) {
-        stop = false;
-        running = false;
-        return
-    }
-    if (
-        (direction > 0 && index + direction === maxIndex) ||
-        (direction < 0 && index === minIndex)
-    ) {
-        if (sorted) {
-            running = false
+    setup: function () {
+        setupArray()
+        window.maxIndex = arr.length;
+        window.minIndex = 0;
+        window.direction = 1;
+        superSetup()
+    },
+
+    next: function () {
+        if (stop) {
+            stop = false;
+            running = false;
             return
         }
-        sorted = true;
-        if (index + direction === maxIndex) {
-            maxIndex--
+        if (
+            (direction > 0 && index + direction === maxIndex) ||
+            (direction < 0 && index === minIndex)
+        ) {
+            if (sorted) {
+                running = false
+                return
+            }
+            sorted = true;
+            if (index + direction === maxIndex) {
+                maxIndex--
+            }
+            if (index === minIndex) {
+                minIndex++
+            }
+            direction *= -1
+            index += direction
+            return setTimeout(cocktail.next, delay)
         }
-        if (index === minIndex) {
-            minIndex++
+        $lis = $("li")
+        if (
+            (arr[index] > arr[index + direction] && direction > 0) ||
+            (arr[index] < arr[index + direction] && direction < 0)
+        ) {
+            sorted = false
+            var c = arr[index]
+            arr[index] = arr[index + direction]
+            arr[index + direction] = c
+            $($lis[index]).css("bottom", arr[index]*multiplier + "px")
+            $($lis[index + direction]).css("bottom", arr[index + direction]*multiplier + "px")
         }
-        direction *= -1
+        $lis.removeClass("current")
+        $($lis[index + direction]).addClass("current")
         index += direction
-        return setTimeout(next, delay)
-    }
-    $lis = $("li")
-    if (
-        (arr[index] > arr[index + direction] && direction > 0) ||
-        (arr[index] < arr[index + direction] && direction < 0)
-    ) {
-        sorted = false
-        var c = arr[index]
-        arr[index] = arr[index + direction]
-        arr[index + direction] = c
-        $($lis[index]).css("bottom", arr[index]*multiplier + "px")
-        $($lis[index + direction]).css("bottom", arr[index + direction]*multiplier + "px")
-    }
-    $lis.removeClass("current")
-    $($lis[index + direction]).addClass("current")
-    index += direction
-    setTimeout(next, delay)
+        setTimeout(cocktail.next, delay)
+    },
 }
